c61193bf54c98157cbfcdf7ed69e0d116aa30a8e
Jeongwoo Nam <wnwjq00123@naver.com>
2023-03-16 16:10:49 +0900

    update files commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,4 +5,5 @@ from django.shortcuts import render
 
 def hello_world(request):
     # return HttpResponse('Hello world!')
-    return render(request, 'base.html')
+    # return render(request, 'base.html')
+    return render(request, 'accountapp/hello_world.html')

0a48b1ea5a615d1e5865eaab14074676357cb027
Jeongwoo Nam <wnwjq00123@naver.com>
2023-03-22 18:42:53 +0900

    decorator code add files commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,39 +1,39 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
+from accountapp.templates.accountapp.decorators import account_ownership_required
 
-
+has_ownership = [account_ownership_required, login_required]
 # Create your views here.
 
+@login_required
 def hello_world(request):
     # 로그인 확인
-    if request.user.is_authenticated:
+    if request.method == "POST":
+        temp = request.POST.get('hello_world_input')
+        # 객체 생성
+        new_hello_world = HelloWorld()
+        # input data hello_world.text 저장
+        new_hello_world.text = temp
+        # model 저장
+        new_hello_world.save()
 
-        if request.method == "POST":
-            temp = request.POST.get('hello_world_input')
-            # 객체 생성
-            new_hello_world = HelloWorld()
-            # input data hello_world.text 저장
-            new_hello_world.text = temp
-            # model 저장
-            new_hello_world.save()
+        hello_world_list = HelloWorld.objects.all()  # 객체에 모든 것을 담는다.
 
-            hello_world_list = HelloWorld.objects.all()  # 객체에 모든 것을 담는다.
-
-            # return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
-        else:
-            hello_world_list = HelloWorld.objects.all()  # 객체에 모든 것을 담는다.
-            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
-            # return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD'})
+        # return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return HttpResponseRedirect(reverse('accountapp:login'))
+        hello_world_list = HelloWorld.objects.all()  # 객체에 모든 것을 담는다.
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+        # return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD'})
 
 
 class AccountCreateView(CreateView):
@@ -51,6 +51,8 @@ class AccountDetailView(DetailView):
     template_name = 'accountapp/detail.html'
 
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User  # 장고 제공 User Class
     context_object_name = 'target_user'
@@ -58,43 +60,11 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
-    def get(self, *args, **kwargs):
 
-        # 로그인 상태 and 현재 객체와 요청 객체 값 비교 (현재 사용자 확인)
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            # 로그인 상태 처리
-            return super().get(*args, **kwargs)
-        else:
-            # 로그아웃 상태 처리
-            return HttpResponseForbidden()
-
-    def post(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            # 로그인 상태 처리
-            return super().post(*args, **kwargs)
-        else:
-            # 로그아웃 상태 처리
-            return HttpResponseForbidden()
-
-
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User  # 장고 제공 User Class
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/delete.html'
-
-    def get(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            # 로그인 상태 처리
-            return super().get(*args, **kwargs)
-        else:
-            # 로그아웃 상태 처리
-            return HttpResponseForbidden()
-
-    def post(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            # 로그인 상태 처리
-            return super().post(*args, **kwargs)
-        else:
-            # 로그아웃 상태 처리
-            return HttpResponseForbidden()

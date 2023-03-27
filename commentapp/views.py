3d6c9bff5406dde5e319a1126203a7e75af07813
Jeongwoo Nam <wnwjq00123@naver.com>
2023-03-27 16:48:06 +0900

    update comment create, delete files commit

--- commentapp/views.py
+++ commentapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, DeleteView
 
 from articleapp.models import Article
+from commentapp.decorators import comment_ownership_required
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
 
@@ -16,10 +18,21 @@ class CommentCreateView(CreateView):
 
     def form_valid(self, form):
         temp_comment = form.save(commit=False)
-        temp_comment.article = Article.object.get(pk=self.request.POST['article_pk'])
+        temp_comment.article = Article.objects.get(pk=self.request.POST['article_pk'])
         temp_comment.writer = self.request.user
         temp_comment.save()
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
+
+
+@method_decorator(comment_ownership_required, 'get')
+@method_decorator(comment_ownership_required, 'post')
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'commentapp/delete.html'
 
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})

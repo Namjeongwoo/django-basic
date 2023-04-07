e70ef600cff43b3fe99e8369fe972f3d43069fc6
Jeongwoo Nam <wnwjq00123@naver.com>
2023-04-07 16:26:59 +0900

    bugfix account create, update view file

--- accountapp/views.py
+++ accountapp/views.py
@@ -19,7 +19,7 @@ has_ownership = [account_ownership_required, login_required]
 class AccountCreateView(CreateView):
     model = User  # 장고 제공 User Class
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:detail')
+    success_url = reverse_lazy('accountapp:login')
     # Class Based View : reverse_lazy()
     # Function Based View : reverse()
     template_name = 'accountapp/create.html'
@@ -43,8 +43,11 @@ class AccountUpdateView(UpdateView):
     model = User  # 장고 제공 User Class
     context_object_name = 'target_user'
     form_class = AccountUpdateForm
-    success_url = reverse_lazy('accountapp:detail')
+    #success_url = reverse_lazy('accountapp:detail')
     template_name = 'accountapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
 
 
 @method_decorator(has_ownership, 'get')

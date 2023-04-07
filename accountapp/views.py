dd28525dedcbc21ae824baf56f24a96038169c23
Jeongwoo Nam <wnwjq00123@naver.com>
2023-04-07 16:15:39 +0900

    bugfix account view file

--- accountapp/views.py
+++ accountapp/views.py
@@ -19,7 +19,7 @@ has_ownership = [account_ownership_required, login_required]
 class AccountCreateView(CreateView):
     model = User  # 장고 제공 User Class
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('accountapp:detail')
     # Class Based View : reverse_lazy()
     # Function Based View : reverse()
     template_name = 'accountapp/create.html'
@@ -43,7 +43,7 @@ class AccountUpdateView(UpdateView):
     model = User  # 장고 제공 User Class
     context_object_name = 'target_user'
     form_class = AccountUpdateForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('accountapp:detail')
     template_name = 'accountapp/update.html'
 
 
